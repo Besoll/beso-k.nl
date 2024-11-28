7573d79497926909965abc34d6929466079a8513
Besik Kavzharadze <b.kavzharadze@gmail.com>
2024-11-28 18:45:44 +0100

    update

--- app/(package)/website-development/page.tsx
+++
@@ -1,8 +0,0 @@
-import ServicePage from "@/components/packages/ServicePage";
-import { servicePagesData } from "@/data";
-
-export default function WebsiteDevelopmentPage() {
-  const websiteDevelopmentData = servicePagesData["website-development"];
-  return <ServicePage {...websiteDevelopmentData} overview={websiteDevelopmentData.overview.join("\n")} />;
-}
-
--- app/(package)/wordpress/page.tsx
+++
@@ -1,7 +0,0 @@
-import ServicePage from "@/components/packages/ServicePage";
-import { servicePagesData } from "@/data";
-
-export default function WordPressPage() {
-  const { wordpress } = servicePagesData;
-  return <ServicePage {...wordpress} />;
-}
