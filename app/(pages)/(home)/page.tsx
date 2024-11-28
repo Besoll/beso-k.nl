52e213c56d62e41574f4607ac100ddfd6ad6f083
Besik Kavzharadze <b.kavzharadze@gmail.com>
2024-11-28 18:30:14 +0100

    update lego spaceship

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -38,7 +38,79 @@ const page = () => {
 
       <Skills />
 
-      <ProjectsSection />
+      <RecentProjects projectItems={projectItems[0]}/>
+
+      <section className='w-full flex justify-center items-centerflex-col h-auto py-16 md:py-32  text-black' >
+        <TestimonialsSmall testimonialSmall={testimonialSmall[0]}/>        
+      </section> 
+
+      <section className='w-full h-auto flex justify-center items-center scroll-smooth md:scroll-auto text-black
+                          smSP:bg-[url("/veranda/v-4.webp")] mdSP:bg-[url("/veranda/v-4.webp")] lgSP:bg-[url("/veranda/v-4.webp")] bg-no-repeat bg-cover bg-center
+                          '>
+        <div className='w-full h-auto flex justify-center items-center bg-MainColor/60 py-16 md:py-32 '>
+          <StatisticsCards statisticSection={statisticSection[0]} statisticsItems={statisticsItems} /> 
+        </div>
+      </section>
+
+
+
+      
+
+      {/* <section className='w-full h-auto flex justify-center items-center py-16 md:py-32  text-black' id='websummit' >
+          <WebSummit  />
+      </section> 
+
+      <section className='w-full h-auto flex justify-center items-center py-16 md:py-32 bg-MainBG text-black' id='pricing' >
+          <Pricing  />
+      </section> 
+
+      <section className='w-full h-auto flex justify-center items-center py-16 md:py-32 text-black' id='about' >
+          <Section1 homeSection1={homeSection1[0]} />
+      </section>
+
+      <section className='w-full h-auto flex justify-center items-center scroll-smooth md:scroll-auto text-white 
+                          bg-MainColor pt-16 md:pt-32' id='deal' >
+          <SectionDeal i={sectionDeal[1]} /> 
+      </section>      
+      
+
+
+      <section className='w-full h-auto flex justify-center items-center py-16 md:py-32 text-black' id='services' >
+          <SectionServices  sectionServices={sectionServices[0]} serviceItems={serviceItems}/>
+      </section>    
+
+      <section className='w-full h-auto flex justify-center items-center py-16 md:py-32 text-black' id='howitworks'>
+          <Section2  howItWorks={howItWorks[0]} howItWorksItems={howItWorksItems}/>
+      </section>
+
+      <section 
+        className='w-full flex justify-center items-center flex-col h-auto py-6 text-black' 
+        id='testimonials' 
+        >
+        <TestimonialsBig testimonialBig={testimonialBig[0]}/>        
+      </section>
+
+
+      <SectionMetrics speedItems={speedItems[0]}/>    
+
+
+      <section className='w-full h-auto flex justify-center items-center scroll-smooth md:scroll-auto text-white 
+                          bg-MainColor pt-16 pb-16 md:pb-0 md:pt-32' >
+          <SectionDeal i={sectionDeal[0]} /> 
+      </section>
+
+      <RecentProjects projectItems={projectItems[0]}/>
+
+
+
+
+      <section className='w-full max-w-[1440px] h-auto flex flex-col md:flex-row justify-center items-center 
+                          gap-8 md:gap-12 py-8 md:py-12     
+                          px-4 md:px-6 pb-16 md:pb-32
+                          scroll-mt-16 snap-start scroll-smooth md:scroll-auto' id='faq'>  
+         
+        <FAQ accordionTitle={accordionTitle[0]} accordionInput={accordionInput} />
+      </section> */}
 
     </main>
   )
--- components/home/Skills.tsx
+++ components/home/Skills.tsx
@@ -1,64 +1,105 @@
 'use client';
 
-import { FC, useEffect } from 'react';
+import { FC, useState } from 'react';
 import { motion } from 'framer-motion';
+import { CardBody, CardContainer, CardItem } from '@/components/ui/3dCard';
 import { FaCode, FaPaintBrush, FaProjectDiagram, FaServer, FaCogs, FaUserTie } from 'react-icons/fa';
+import Image from 'next/image';
 
 const SkillsSection: FC = () => {
+  const [touched, setTouched] = useState(false);
+
+  const handleTouch = () => {
+    setTouched(true);
+    setTimeout(() => setTouched(false), 1000); // Reset the touched state after 1 second
+  };
+
   const skills = [
     {
-      icon: <FaCode className="w-16 h-16 text-owlGreen-500" />, 
+      icon: <FaCode className="w-16 h-16 text-owlGreen-500" />,
       title: 'Technical Skills',
       description: 'Expert in JavaScript (Node.js, ReactJS), PHP, HTML, CSS, SQL, XML, Front-End & Back-End frameworks (ReactJS, Next.js, TailwindCSS, Laravel, Express.js). Skilled in cloud platforms like AWS, Vercel, and Google Cloud.',
     },
     {
-      icon: <FaPaintBrush className="w-16 h-16 text-owlGreen-500" />, 
+      icon: <FaPaintBrush className="w-16 h-16 text-owlGreen-500" />,
       title: 'Creative Skills',
       description: 'Experienced in Adobe Creative Suite (Photoshop, Illustrator, After Effects), Figma, CorelDRAW, DaVinci Resolve Studio. Skilled in web and graphic design, with a knack for creating visually appealing digital content.',
     },
     {
-      icon: <FaProjectDiagram className="w-16 h-16 text-owlGreen-500" />, 
+      icon: <FaProjectDiagram className="w-16 h-16 text-owlGreen-500" />,
       title: 'Project Management',
       description: 'Proficient in Agile methodologies, strategic planning, resource and budget management, and team empowerment. Focused on delivery with a creative problem-solving approach.',
     },
     {
-      icon: <FaServer className="w-16 h-16 text-owlGreen-500" />, 
+      icon: <FaServer className="w-16 h-16 text-owlGreen-500" />,
       title: 'DevOps & Cloud',
       description: 'Experienced with DevOps tools like GitHub, Jenkins, Docker, Terraform, and cloud services like AWS (EC2, S3, RDS), Vercel, and Google Cloud. Focus on scalable and efficient product delivery.',
     },
     {
-      icon: <FaCogs className="w-16 h-16 text-owlGreen-500" />, 
+      icon: <FaCogs className="w-16 h-16 text-owlGreen-500" />,
       title: 'Automation & AI',
       description: 'Expertise in Generative AI, automation solutions, and AI-driven marketplace listings. Skilled in building automated systems for improved scalability and performance.',
     },
     {
-      icon: <FaUserTie className="w-16 h-16 text-owlGreen-500" />, 
+      icon: <FaUserTie className="w-16 h-16 text-owlGreen-500" />,
       title: 'Leadership & Strategy',
       description: 'Strong background in entrepreneurial leadership, strategic planning, and stakeholder management. Proven ability to inspire teams and align cross-functional synergies for business growth.',
     },
   ];
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const skillsSection = document.getElementById('skills');
-      if (skillsSection) {
-        const rect = skillsSection.getBoundingClientRect();
-        if (rect.top < window.innerHeight && rect.bottom > 0) {
-          document.body.classList.add('dark-mode');
-        } else {
-          document.body.classList.remove('dark-mode');
-        }
-      }
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
   return (
-    <section id="skills" className="relative w-screen h-screen bg-gradient-to-b from-owlGreen-900 to-black text-center overflow-hidden">
-      <div className="container mx-auto h-full p-6 flex flex-col justify-center items-center">
+    <section
+      id="skills"
+      className="relative w-full py-20 bg-gradient-to-b from-white to-white text-center overflow-hidden"
+    >
+      <div className="relative mx-auto w-[300px] h-[300px]">
+        <motion.div
+          initial={{ y: 0 }}
+          animate={{ y: touched ? 0 : [0, -20, 0], rotate: touched ? 0 : [0, 10, -10, 0] }}
+          transition={touched ? { duration: 4, ease: 'easeInOut' } : { repeat: Infinity, duration: 3, ease: 'easeInOut' }}
+          whileHover={{ scale: 1.1, rotate: 5 }}
+          onClick={handleTouch}
+          className="cursor-pointer relative"
+        >
+          <motion.div
+            animate={
+              touched
+                ? { x: ['0%', '5%', '10%', '15%', '20%', '25%', '30%', '35%', '40%', '45%', '50%', '55%', '60%', '65%', '70%', '75%', '80%', '85%', '90%', '95%', '100%', '105%', '110%', '115%', '120%', '125%', '130%', '135%', '140%', '145%', '150%', '155%', '160%', '165%', '170%', '175%', '180%', '185%', '190%', '195%', '200%', '205%', '210%', '215%', '220%', '225%', '230%', '235%', '240%', '245%', '250%'], translate: -1500, transition: { x: ['0%', '5%', '10%', '15%', '20%', '25%', '30%', '35%', '40%', '45%', '50%', '55%', '60%', '65%', '70%', '75%', '80%', '85%', '90%', '95%', '100%', '105%', '110%', '115%', '120%', '125%', '130%', '135%', '140%', '145%', '150%', '155%', '160%', '165%', '170%', '175%', '180%', '185%', '190%', '195%', '200%', '205%', '210%', '215%', '220%', '225%', '230%', '235%', '240%', '245%', '250%'], duration: 4800000, ease: 'linear' }, transitionEnd: { x: 0 } }
+                : {}
+            }
+            transition={{ duration: 60, ease: ['easeIn', 'easeOut', 'easeInOut'] }}
+          >
+            <Image
+              src='/Andri-Space-ship-1.webp'
+              alt='Andri Space Ship lego 1'
+              width={400}
+              height={400}
+              className='object-cover transition-transform duration-500 ease-in-out'
+            />
+          </motion.div>
+          <motion.div
+            className='absolute inset-0 bg-owlGreen-500 opacity-20 blur-xl rounded-full'
+            initial={{ scale: 1 }}
+            animate={{ scale: [1, 1.1, 1], opacity: [0.2, 0.3, 0.2] }}
+            transition={{ repeat: Infinity, duration: 4, ease: 'easeInOut' }}
+          />
+          {touched && (
+            <motion.div
+              className='absolute inset-0 flex justify-center items-center'
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: [1, 1.5, 1], opacity: [0, 1, 0] }}
+              transition={{ duration: 0.6 }}
+            >
+              <div className='bg-white text-owlGreen-800 font-bold p-4 rounded-full shadow-lg'>
+                🚀 Woosh!
+              </div>
+            </motion.div>
+          )}
+        </motion.div>
+      </div>
+      <div className="container mx-auto p-6 mt-12">
         <motion.h2
-          className="text-5xl md:text-6xl font-bold text-white mb-8"
+          className="text-4xl md:text-5xl font-bold text-owlGreen-800"
           initial={{ opacity: 0, y: -50 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
@@ -67,7 +108,7 @@ const SkillsSection: FC = () => {
           My Skills
         </motion.h2>
         <motion.p
-          className="text-lg md:text-xl text-gray-300 max-w-3xl mx-auto mb-16"
+          className="text-lg md:text-xl mt-4 max-w-3xl mx-auto text-gray-700 font-bold"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
@@ -75,38 +116,46 @@ const SkillsSection: FC = () => {
         >
           I bring a diverse set of skills to the table, spanning technical expertise, creative design, project management, DevOps, AI integration, and leadership. Here are the key areas where I excel:
         </motion.p>
-        <motion.div
-          className="flex gap-16 w-full h-[70%] overflow-x-scroll snap-x snap-mandatory scrollbar-hide"
-          whileTap={{ cursor: 'grabbing' }}
-        >
+        <div className="flex flex-wrap justify-center items-center gap-4 mt-12">
           {skills.map((skill, index) => (
-            <motion.div
-              key={index}
-              className="snap-center min-w-[400px] h-[450px] p-10 bg-gradient-to-tr from-owlGreen-500 to-blue-700 text-white rounded-[10%] shadow-xl transform-gpu hover:scale-110 hover:skew-y-3 transition duration-700 ease-in-out"
-              initial={{ opacity: 0, scale: 0.8 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-              viewport={{ once: true }}
-              transition={{ delay: index * 0.3, duration: 1 }}
-            >
-              <div className="flex justify-center mb-6">{skill.icon}</div>
-              <h3 className="text-3xl font-bold text-white mb-4">{skill.title}</h3>
-              <p className="text-lg text-gray-100">{skill.description}</p>
-            </motion.div>
+            <CardContainer key={index} className="inter-var hover:z-50 shadow-lg rounded-xl flex 1 group">
+              <motion.div
+                whileHover={{ y: -10, scale: 1.05 }}
+                transition={{ type: 'spring', stiffness: 150, damping: 10 }}
+                className='relative'
+              >
+                <CardBody className="bg-owlGreen-100 relative group/card hover:shadow-2xl hover:shadow-emerald-500/[0.1] border-white/[0.2] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border min-h-[550px] flex justify-center items-center flex-col">
+                  <CardItem
+                    translateZ="50"
+                    className="px-4 text-2xl font-bold text-neutral-600"
+                  >
+                    {skill.title}
+                  </CardItem>
+                  <CardItem
+                    translateZ="100"
+                    rotateX={30}
+                    rotateZ={-10}
+                    className="w-full mt-4 flex justify-center"
+                  >
+                    {skill.icon}
+                  </CardItem>
+                  <CardItem
+                    as="p"
+                    translateZ="60"
+                    className="px-4 text-neutral-500 font-regular text-lg max-w-sm mt-12"
+                  >
+                    {skill.description}
+                  </CardItem>
+                </CardBody>
+                <motion.div
+                  className='absolute -inset-1 bg-emerald-500 opacity-0 blur-md rounded-lg'
+                  whileHover={{ opacity: 0.4 }}
+                  transition={{ duration: 0.4, ease: 'easeInOut' }}
+                />
+              </motion.div>
+            </CardContainer>
           ))}
-        </motion.div>
-        {/* Parallax Background Animation */}
-        <motion.div
-          className="absolute top-0 left-0 w-[40%] h-[40%] bg-gradient-to-r from-blue-600 to-purple-600 rounded-full opacity-50 blur-3xl pointer-events-none animate-pulse"
-          initial={{ y: -100 }}
-          animate={{ y: [0, -50, 0] }}
-          transition={{ duration: 12, repeat: Infinity, ease: 'easeInOut' }}
-        ></motion.div>
-        <motion.div
-          className="absolute bottom-0 right-0 w-[35%] h-[35%] bg-gradient-to-l from-purple-500 to-pink-500 rounded-full opacity-40 blur-3xl pointer-events-none animate-pulse"
-          initial={{ y: 100 }}
-          animate={{ y: [0, 50, 0] }}
-          transition={{ duration: 12, repeat: Infinity, ease: 'easeInOut' }}
-        ></motion.div>
+        </div>
       </div>
     </section>
   );
