6924cc2ce269bfe75e320f5876e462cfb28ef202
Besik Kavzharadze <b.kavzharadze@gmail.com>
2024-11-28 18:42:00 +0100

    update

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,25 +1,25 @@
 
-import { accordionInput, accordionTitle, formItems, heroExtend, heroItems, homeSection1, howItWorks, howItWorksItems, projectItems, sectionDeal, sectionServices, serviceItems, speedItems, statisticSection, statisticsItems, testimonialBig, testimonialSmall,  } from '@/data'
+import {  projectItems, statisticSection, statisticsItems, testimonialSmall,  } from '@/data'
 import { LogoCarousel } from '@/components/global/LogoCarousel'
 import dynamic from 'next/dynamic'
 import AboutMe from '@/components/home/AboutMe'
 import Skills from '@/components/home/Skills'
-import ProjectsSection from '@/components/home/Projects'
+// import ProjectsSection from '@/components/home/Projects'
 const Hero = dynamic(() => import("@/components/home/Hero"))
-const HeroExtend = dynamic(() => import('@/components/home/HeroExtend')) 
-const FormSection = dynamic(() => import('@/components/home/FormSection')) 
-const Section1 = dynamic(() => import("@/components/home/Section1"))
-const SectionDeal = dynamic(() => import("@/components/home/SectionDeal"))
-const SectionServices = dynamic(() => import("@/components/home/SectionServices"))
-const Section2 = dynamic(() => import("@/components/home/Section2"))
-const TestimonialsBig = dynamic(() => import("@/components/home/TestimonialsBig"))
-const SectionMetrics = dynamic(() => import('@/components/home/SectionMetrics')) 
+// const HeroExtend = dynamic(() => import('@/components/home/HeroExtend')) 
+// const FormSection = dynamic(() => import('@/components/home/FormSection')) 
+// const Section1 = dynamic(() => import("@/components/home/Section1"))
+// const SectionDeal = dynamic(() => import("@/components/home/SectionDeal"))
+// const SectionServices = dynamic(() => import("@/components/home/SectionServices"))
+// const Section2 = dynamic(() => import("@/components/home/Section2"))
+// const TestimonialsBig = dynamic(() => import("@/components/home/TestimonialsBig"))
+// const SectionMetrics = dynamic(() => import('@/components/home/SectionMetrics')) 
 const RecentProjects = dynamic(() => import('@/components/home/RecentProjects')) 
 const TestimonialsSmall = dynamic(() => import("@/components/home/TestimonialsSmall"))
 const StatisticsCards = dynamic(() => import("@/components/home/StatisticsCards"))
-const Pricing = dynamic(() => import("@/components/home/Pricing"))
-const WebSummit = dynamic(() => import("@/components/home/WebSummit"))
-const FAQ = dynamic(() => import("@/components/home/FAQ"))
+// const Pricing = dynamic(() => import("@/components/home/Pricing"))
+// const WebSummit = dynamic(() => import("@/components/home/WebSummit"))
+// const FAQ = dynamic(() => import("@/components/home/FAQ"))
 
 
 const page = () => {
