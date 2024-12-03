c1cf2fee5dd157078063c2c3d7d9bfe82b8e61fb
Besik Kavzharadze <b.kavzharadze@gmail.com>
2024-12-03 13:34:28 +0100

    add custom events on download buttons

--- components/global/DownloadCV.tsx
+++ components/global/DownloadCV.tsx
@@ -14,9 +14,22 @@ const DownloadCV = ({
         onClick={() => {
             const password = prompt('Please enter the password to download the CV. If you do not have the password, feel free to request it using the contact form below:');
             if (password === downloadPasswordForCV) {
+
+                window.dataLayer = window.dataLayer || [];
+                window.dataLayer.push({
+                    event: 'cv_download_success',
+                    formURL: window.location.href
+                });
                 window.location.href = '/Beso_Kavzharadze_CV.pdf';
             } else {
                 alert('Incorrect password. If you need access, please reach out to me through the contact form to request the correct password.');
+
+                window.dataLayer = window.dataLayer || [];
+                window.dataLayer.push({
+                    event: 'cv_download_failed',
+                    formURL: window.location.href
+                });
+                
             }
             }}
     >
